62e78dcce361b327dff6054902c36e6099d278aa
t.nugent <t.nugent@hotmail.co.uk>
2023-11-27 16:52:11 +0000

    Adding attributes and models to represent many:many relationships

---
+++ learn-Pokemon-Review-App/Models/PokemonOwner.cs
@@ -0,0 +1,10 @@
+﻿namespace learn_Pokemon_Review_App.Models
+{
+    public class PokemonOwner
+    {
+        public int PokemonId { get; set; }
+        public int OwnerId { get; set; }
+        public Pokemon Pokemon { get; set; }
+        public Owner Owner { get; set; }
+    }
+}
