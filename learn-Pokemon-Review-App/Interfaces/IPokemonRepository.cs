887a67adb73ae233735277962ffd846b5b0bd496
t.nugent <t.nugent@hotmail.co.uk>
2023-11-28 16:57:34 +0000

    Extending IPokemonRepository, implementing in PokemonRepository

--- learn-Pokemon-Review-App/Interfaces/IPokemonRepository.cs
+++ learn-Pokemon-Review-App/Interfaces/IPokemonRepository.cs
@@ -5,5 +5,10 @@ namespace learn_Pokemon_Review_App.Interfaces
     public interface IPokemonRepository
     {
         ICollection<Pokemon> GetPokemons();
+        Pokemon GetPokemon(int id);
+        Pokemon GetPokemon(string name);
+        decimal GetPokemonRating(int pokeId);
+        bool PokemonExists(int pokeId);
+
     }
 }
