7f5eadd04615441f6b85e3f6b3a66d24ac55de42
t.nugent <t.nugent@hotmail.co.uk>
2023-11-29 17:26:07 +0000

    CountryRepository

---
+++ learn-Pokemon-Review-App/Repositories/CountryRepository.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using learn_Pokemon_Review_App.Data;
+using learn_Pokemon_Review_App.Interfaces;
+using learn_Pokemon_Review_App.Models;
+
+namespace learn_Pokemon_Review_App.Repositories
+{
+    public class CountryRepository : ICountryRepository
+    {
+        private readonly IMapper _mapper;
+        private readonly DataContext _context;
+        public CountryRepository(DataContext context, IMapper mapper)
+        {
+            _mapper = mapper;
+            _context = context;
+            // Bring in data context
+        }
+        public bool CountryExists(int id)
+        {
+            return _context.Countries.Any(c => c.Id == id);
+        }
+
+        public ICollection<Country> GetCountries()
+        {
+            return _context.Countries.ToList();
+        }
+
+        public Country GetCountry(int id)
+        {
+            return _context.Countries.Where(c => c.Id == id).FirstOrDefault();
+        }
+
+        public Country GetCountryByOwner(int ownerId)
+        {
+            return _context.Owners.
+                Where(o => o.Id == ownerId)
+                .Select(c => c.Country).FirstOrDefault();
+        }
+
+        public ICollection<Owner> GetOwnersFromACountry(int countryId)
+        {
+            return _context.Owners.Where(c => c.Country.Id == countryId).ToList();
+        }
+    }
+}
