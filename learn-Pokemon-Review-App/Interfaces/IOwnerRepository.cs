63d559cf556e43a205d63afbcd01faa362118bbc
t.nugent <t.nugent@hotmail.co.uk>
2023-11-29 18:27:42 +0000

    IOwnerRepository interface

---
+++ learn-Pokemon-Review-App/Interfaces/IOwnerRepository.cs
@@ -0,0 +1,15 @@
+﻿using learn_Pokemon_Review_App.Models;
+
+namespace learn_Pokemon_Review_App.Interfaces
+{
+    public interface IOwnerRepository
+    {
+        ICollection<Owner> GetOwners();
+        Owner GetOwner(int ownerId);
+        ICollection<Owner> GetOwnerOfAPokemon(int pokeId);
+        ICollection<Pokemon> GetPokemonByOwner(int ownerId);
+        bool OwnerExists(int ownerId);
+
+
+    }
+}
