ffabbf47bb2efcfa98c01725053d4a1e511e1ea0
t.nugent <t.nugent@hotmail.co.uk>
2023-11-29 18:28:34 +0000

    OwnerDto

---
+++ learn-Pokemon-Review-App/Dto/OwnerDto.cs
@@ -0,0 +1,13 @@
+﻿using learn_Pokemon_Review_App.Models;
+
+namespace learn_Pokemon_Review_App.Dto
+{
+    public class OwnerDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gym { get; set; }
+        public Country Country { get; set; }
+    }
+}
