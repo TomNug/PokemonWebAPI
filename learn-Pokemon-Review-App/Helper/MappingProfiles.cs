dfadbaa72b6c718c8c2ee17e6195907e30e0ffd9
t.nugent <t.nugent@hotmail.co.uk>
2023-11-30 15:53:51 +0000

    Update mapping for review and reviewer

--- learn-Pokemon-Review-App/Helper/MappingProfiles.cs
+++ learn-Pokemon-Review-App/Helper/MappingProfiles.cs
@@ -12,6 +12,8 @@ namespace learn_Pokemon_Review_App.Helper
             CreateMap<Category, CategoryDto>();
             CreateMap<Country, CountryDto>();
             CreateMap<Owner, OwnerDto>();
+            CreateMap<Review, ReviewDto>();
+            CreateMap<Reviewer, ReviewerDto>();
         }
     }
 }
