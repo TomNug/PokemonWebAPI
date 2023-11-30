79badaf8163be71c3b93be3c5cd109bc9e766002
t.nugent <t.nugent@hotmail.co.uk>
2023-11-30 15:31:54 +0000

    Added ReviewerDto

---
+++ learn-Pokemon-Review-App/Dto/ReviewerDto.cs
@@ -0,0 +1,9 @@
+﻿namespace learn_Pokemon_Review_App.Dto
+{
+    public class ReviewerDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
