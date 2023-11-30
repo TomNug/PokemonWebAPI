fda7c8154c0b2c84dd93985f838eb3104f31bfd6
t.nugent <t.nugent@hotmail.co.uk>
2023-11-30 15:25:19 +0000

    Fixed return type to Pokemon

---
+++ learn-Pokemon-Review-App/Repositories/ReviewerRepository.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using learn_Pokemon_Review_App.Data;
+using learn_Pokemon_Review_App.Interfaces;
+using learn_Pokemon_Review_App.Models;
+
+namespace learn_Pokemon_Review_App.Repositories
+{
+    public class ReviewerRepository : IReviewerRepository
+    {
+        private readonly DataContext _context;
+        private readonly IMapper _mapper;
+
+        public ReviewerRepository(DataContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        public Reviewer GetReviewer(int reviewerId)
+        {
+            return _context.Reviewers.Where(
+                r => r.Id == reviewerId).FirstOrDefault();
+        }
+
+        public ICollection<Reviewer> GetReviewers()
+        {
+            return _context.Reviewers.ToList();
+        }
+
+        public ICollection<Review> GetReviewsByReviewer(int reviewerId)
+        {
+            return _context.Reviews.Where(
+                r => r.Reviewer.Id == reviewerId).ToList();
+        }
+
+        public bool ReviewerExists(int reviewerId)
+        {
+            return _context.Reviewers.Any(r => r.Id == reviewerId);
+        }
+    }
+}
