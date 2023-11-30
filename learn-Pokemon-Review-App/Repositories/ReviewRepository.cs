e63d91bcfaf5222fd203789e58e298e5075a9797
t.nugent <t.nugent@hotmail.co.uk>
2023-11-30 13:47:48 +0000

    Implementing ReviewRepository

--- learn-Pokemon-Review-App/Repositories/ReviewRepository.cs
+++ learn-Pokemon-Review-App/Repositories/ReviewRepository.cs
@@ -1,28 +1,40 @@
-﻿using learn_Pokemon_Review_App.Interfaces;
+﻿using AutoMapper;
+using learn_Pokemon_Review_App.Data;
+using learn_Pokemon_Review_App.Interfaces;
 using learn_Pokemon_Review_App.Models;
 
 namespace learn_Pokemon_Review_App.Repositories
 {
     public class ReviewRepository : IReviewRepository
     {
+        private readonly DataContext _context;
+        private readonly IMapper _mapper;
+
+        public ReviewRepository(DataContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
         public Review GetReview(int reviewId)
         {
-            throw new NotImplementedException();
+            return _context.Reviews.Where(
+                r => r.Id == reviewId).FirstOrDefault();
         }
 
         public ICollection<Review> GetReviews()
         {
-            throw new NotImplementedException();
+            return _context.Reviews.OrderBy(r => r.Id).ToList();
         }
 
         public ICollection<Review> GetReviewsOfAPokemon(int pokeId)
         {
-            throw new NotImplementedException();
+            return _context.Reviews.Where(r => r.Pokemon.Id == pokeId).ToList();
         }
 
         public bool ReviewExists(int reviewId)
         {
-            throw new NotImplementedException();
+            return _context.Reviews.Any(r => r.Id == reviewId);
         }
     }
 }
