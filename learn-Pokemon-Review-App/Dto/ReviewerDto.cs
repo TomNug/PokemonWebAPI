88cc7416dfbd15b22d95828abe54d7827755be10
t.nugent <t.nugent@hotmail.co.uk>
2023-11-30 15:54:42 +0000

    Implementing reviewerController

--- learn-Pokemon-Review-App/Dto/ReviewerDto.cs
+++ learn-Pokemon-Review-App/Dto/ReviewerDto.cs
@@ -1,9 +1,12 @@
-﻿namespace learn_Pokemon_Review_App.Dto
+﻿using learn_Pokemon_Review_App.Models;
+
+namespace learn_Pokemon_Review_App.Dto
 {
     public class ReviewerDto
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public ICollection<Review> Reviews { get; set; }
     }
 }
