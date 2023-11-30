69bebe3e47b729de8d1d6a715e848c87830a4bbe
t.nugent <t.nugent@hotmail.co.uk>
2023-11-30 12:59:16 +0000

    OwnerController

---
+++ learn-Pokemon-Review-App/Controllers/OwnerController.cs
@@ -0,0 +1,66 @@
+﻿using AutoMapper;
+using learn_Pokemon_Review_App.Dto;
+using learn_Pokemon_Review_App.Interfaces;
+using learn_Pokemon_Review_App.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace learn_Pokemon_Review_App.Controllers
+{
+    // Attributes to turn them into controllers
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OwnerController : Controller
+    {
+        private readonly IOwnerRepository _ownerRepository;
+        private readonly IMapper _mapper;
+
+        public OwnerController(IOwnerRepository ownerRepository, IMapper mapper)
+        {
+            _ownerRepository = ownerRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Owner>))]
+        public IActionResult GetOwners()
+        {
+            var owners = _mapper.Map<List<OwnerDto>>(_ownerRepository.GetOwners());
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            return Ok(owners);
+        }
+
+        [HttpGet("{ownerId}")]
+        [ProducesResponseType(200, Type = typeof(Owner))]
+        [ProducesResponseType(400)]
+        public IActionResult GetOwner(int ownerId)
+        {
+            if (!_ownerRepository.OwnerExists(ownerId))
+                return NotFound();
+
+            var owner = _mapper.Map<OwnerDto>(_ownerRepository.GetOwner(ownerId));
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(owner);
+        }
+
+        [HttpGet("{ownerId}/pokemon")]
+        [ProducesResponseType(200, Type = typeof(Owner))]
+        [ProducesResponseType(400)]
+        public IActionResult GetPokemonByOwner(int ownerId)
+        {
+            if (!_ownerRepository.OwnerExists(ownerId))
+                return NotFound();
+            var owner = _mapper.Map<List<PokemonDto>>(
+                _ownerRepository.GetPokemonByOwner(ownerId));
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(owner);
+        }
+    }
+}
