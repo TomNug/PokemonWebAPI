f2377323e702a92b012ce70922ede913c42acb98
t.nugent <t.nugent@hotmail.co.uk>
2023-11-30 12:41:13 +0000

    OwnerRepository implementing IOwnerRepository

---
+++ learn-Pokemon-Review-App/Repositories/OwnerRepository.cs
@@ -0,0 +1,48 @@
+﻿using learn_Pokemon_Review_App.Data;
+using learn_Pokemon_Review_App.Interfaces;
+using learn_Pokemon_Review_App.Models;
+
+namespace learn_Pokemon_Review_App.Repositories
+{
+    public class OwnerRepository : IOwnerRepository
+    {
+        private DataContext _context;
+
+        public OwnerRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public Owner GetOwner(int ownerId)
+        {
+            return _context.Owners.Where(
+                o => o.Id == ownerId).FirstOrDefault();
+        }
+
+        public ICollection<Owner> GetOwnerOfAPokemon(int pokeId)
+        {
+            // Need select/include
+            // Entity framework does not bring in navigation properties
+            return _context.PokemonOwners.Where(
+                p => p.Pokemon.Id == pokeId)
+                .Select(o => o.Owner).ToList();
+        }
+
+        public ICollection<Owner> GetOwners()
+        {
+            return _context.Owners.ToList();
+        }
+
+        public ICollection<Pokemon> GetPokemonByOwner(int ownerId)
+        {
+            return _context.PokemonOwners.Where(
+                o => o.OwnerId == ownerId)
+                .Select(p => p.Pokemon).ToList();
+        } // Potential error, changed o to p
+
+        public bool OwnerExists(int ownerId)
+        {
+            return _context.Owners.Any(o => o.Id == ownerId);
+        }
+    }
+}
