3e90224438d2508a6512d0e48913aedc57bd600f
t.nugent <t.nugent@hotmail.co.uk>
2023-11-30 15:05:02 +0000

    Adding IReviewerRepository

---
+++ learn-Pokemon-Review-App/Interfaces/IReviewerRepository.cs
@@ -0,0 +1,12 @@
+﻿using learn_Pokemon_Review_App.Models;
+
+namespace learn_Pokemon_Review_App.Interfaces
+{
+    public interface IReviewerRepository
+    {
+        ICollection<Reviewer> GetReviewers();
+        Reviewer GetReviewer(int reviewerId);
+        ICollection<Review> GetReviewsByReviewer(int reviewerId);
+        bool ReviewerExists();
+    }
+}
