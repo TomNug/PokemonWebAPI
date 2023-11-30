357fc7ba5aff30a00e39fcd7fd1b7cf06201f2f4
t.nugent <t.nugent@hotmail.co.uk>
2023-11-30 13:37:21 +0000

    added IReviewRepository

---
+++ learn-Pokemon-Review-App/Interfaces/IReviewRepository.cs
@@ -0,0 +1,13 @@
+﻿using learn_Pokemon_Review_App.Models;
+
+namespace learn_Pokemon_Review_App.Interfaces
+{
+    public interface IReviewRepository
+    {
+        ICollection<Review> GetReviews();
+        Review GetReview(int reviewId);
+        ICollection<Review> GetReviewsOfAPokemon(int pokeId);
+        bool ReviewExists(int reviewId);
+
+    }
+}
