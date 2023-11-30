c56e05e7b8c549f92ad37d8a45567bb20ee3969b
t.nugent <t.nugent@hotmail.co.uk>
2023-11-30 13:38:26 +0000

    Added ReviewRepository

---
+++ learn-Pokemon-Review-App/Repositories/ReviewRepository.cs
@@ -0,0 +1,28 @@
+﻿using learn_Pokemon_Review_App.Interfaces;
+using learn_Pokemon_Review_App.Models;
+
+namespace learn_Pokemon_Review_App.Repositories
+{
+    public class ReviewRepository : IReviewRepository
+    {
+        public Review GetReview(int reviewId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Review> GetReviews()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Review> GetReviewsOfAPokemon(int pokeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ReviewExists(int reviewId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
