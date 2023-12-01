7fcf7c72c693665331147e899ab759925297d691
t.nugent <t.nugent@hotmail.co.uk>
2023-12-01 15:21:02 +0000

    Implement CreateCountry, Save

--- learn-Pokemon-Review-App/Repositories/CountryRepository.cs
+++ learn-Pokemon-Review-App/Repositories/CountryRepository.cs
@@ -20,6 +20,12 @@ namespace learn_Pokemon_Review_App.Repositories
             return _context.Countries.Any(c => c.Id == id);
         }
 
+        public bool CreateCountry(Country country)
+        {
+            _context.Add(country);
+            return Save();
+        }
+
         public ICollection<Country> GetCountries()
         {
             return _context.Countries.ToList();
@@ -40,6 +46,12 @@ namespace learn_Pokemon_Review_App.Repositories
         public ICollection<Owner> GetOwnersFromACountry(int countryId)
         {
             return _context.Owners.Where(c => c.Country.Id == countryId).ToList();
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
         }
     }
 }
