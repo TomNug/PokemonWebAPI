587648d8f5f84f0d2656591b20d7cebe52f4dac1
t.nugent <t.nugent@hotmail.co.uk>
2023-12-01 14:30:43 +0000

    Updating CategoryRepository with expanded interface

--- learn-Pokemon-Review-App/Repositories/CategoryRepository.cs
+++ learn-Pokemon-Review-App/Repositories/CategoryRepository.cs
@@ -16,6 +16,17 @@ namespace learn_Pokemon_Review_App.Repositories
             return _context.Categories.Any(c => c.Id == id);
         }
 
+        public bool CreateCategory(Category category)
+        {
+            //Change Tracker
+            // add, updating, modifying
+            // connected (90%) vs disconnected (uncommon)
+            // EntityState.Added => using disconnected state
+            _context.Add(category);
+
+            return Save();
+        }
+
         public ICollection<Category> GetCategories()
         {
             return _context.Categories.ToList();
@@ -29,6 +40,14 @@ namespace learn_Pokemon_Review_App.Repositories
         public ICollection<Pokemon> GetPokemonByCategory(int categoryId)
         {
             return _context.PokemonCategories.Where(e => e.CategoryId == categoryId).Select(c => c.Pokemon).ToList();
+        }
+
+        public bool Save()
+        {
+            // _context.SaveChanges() takes the changes
+            // convert it into SQL, and send to DB
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
         }
     }
 }
